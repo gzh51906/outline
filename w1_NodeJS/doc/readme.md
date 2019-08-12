672a63678ab09aa95f4699d670856a3218c52151
aaron <jeroo@qq.com>
2019-08-12 11:29:29 +0800

    day1-1a NodeJS 静态资源服务器

--- w1_NodeJS/doc/readme.md
+++ w1_NodeJS/doc/readme.md
@@ -11,6 +11,28 @@
     * path
     * ...
 * 模块化开发规范
-    * commonJS      NodeJS
-    * AMD           requireJS
-    * CMD           seaJS（不维护）
+    * commonJS      NodeJS（同步）
+        ```js
+            const http = require('http');
+            const app = http.createServer(()=>{
+
+            })
+        ```
+    * AMD           requireJS（异步:预加载）
+        ```js
+            let jq = require(['jquery'],($)=>{
+                $('body')
+            });
+            console.log(jq);// undefined
+        ```
+    * CMD           seaJS（异步:延迟加载，不维护）
+        ```js
+            define(()=>{
+                let $ = require('jquery')
+            })
+        ```
+* 了解前端（客户端）与后端（服务器）
+    * requet        请求（客户端->服务端）
+    * response      响应（服务端->客户端）
+* 静态资源服务器
+    * mime类型
--- w1_NodeJS/src/server_static.js
+++ w1_NodeJS/src/server_static.js
@@ -8,7 +8,18 @@
  */
 // 内置模块（nodejs自带的模块，直接使用）
  const http = require('http');// commonJS同步
+
+ // 用于读取文件
  const fs = require('fs');
+
+ // 引入url模块，用于url地址格式化
+let url = require('url');
+
+// 用于格式化文件地址
+let path = require('path');
+
+// 自定义模块
+let mime = require('./mime');
 
  // 创建一个服务器
 const app = http.createServer((req,res)=>{
@@ -19,17 +30,42 @@ const app = http.createServer((req,res)=>{
     // 1. 通过fs模块读取index.html内容
     // 2. 通过res.write把内容响应到前端
     
-    fs.readFile('./index.html',(err,content)=>{
+    // fs.readFile('./index.html',(err,content)=>{
+    //     // err:错误信息，默认：null
+
+    //     res.write(content);
+
+    //     //结束响应
+    //     res.end();
+    // })
+
+    // 静态资源服务器：根据不同的请求，返回不同的内容
+    let reqObj = url.parse(req.url,true);
+
+    // 得到地址格式：/img/jingjing.jpg
+    let filePath = reqObj.pathname;
+
+    // 获取后缀名.png,.jpg,.html,.css
+    let extname = path.extname(filePath).slice(1);
+
+    // 获取真实路径
+    // 把/img/jingjing.jpg -> D:/myweb/img/jingjing.jpg
+    // __dirname：当前文件所在目录
+    let realPath = path.join(__dirname,filePath);
+
+
+    fs.readFile(realPath,(err,content)=>{
         // err:错误信息，默认：null
+        console.log(err,content)
+
+        // 告诉浏览器内容类型是什么（响应头Content-Type）
+        res.writeHead(200,{'content-type':mime[extname] + ';charset=utf8'});
 
         res.write(content);
 
         //结束响应
         res.end();
     })
-
-    // 根据不同的请求，返回不同的内容
-    
     
 });
 
