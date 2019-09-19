64bd25a184a576cb4de3beb8258f577c74a3e9d5
aaron <jeroo@qq.com>
2019-09-19 16:26:30 +0800

    day6-4p 脚手架与项目要求

---
+++ w6_ReactProject/initapp/config-overrides.js
@@ -0,0 +1,13 @@
+module.exports = function override(config, env) {
+    // 修改配置
+    // config.resolve.alias['@'] = path.join(__dirname,'./src/')
+    // config.resolve.alias['@@'] = path.join(__dirname,'./src/components')
+    // config.resolve.alias['~'] = path.join(__dirname,'./src/pages')
+
+    Object.assign(config.resolve.alias,{
+        '@':path.join(__dirname,'./src/'),
+        '@@':path.join(__dirname,'./src/components'),
+        '~':path.join(__dirname,'./src/pages')
+    })
+    return config;
+}
