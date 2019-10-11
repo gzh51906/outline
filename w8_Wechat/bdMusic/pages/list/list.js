e23969c4ef28fa452c33fe00345c453aca3241a6
aaron <jeroo@qq.com>
2019-10-11 10:38:56 +0800

    day8-5a 列表页 滚动加载

--- w8_Wechat/bdMusic/pages/list/list.js
+++ w8_Wechat/bdMusic/pages/list/list.js
@@ -1,18 +1,63 @@
 // pages/list/list.js
+const regeneratorRuntime = require("regenerator-runtime");
+let app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    page:1,
+    size:10,
+    datalist:[],
+    type:null,
+    loading:false,
+    hasMore:true
+  },
+
+  getData({ type, size = 10, offset = 0 } = {}) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: 'http://tingapi.ting.baidu.com/v1/restserver/ting',
+        data: {
+          method: 'baidu.ting.billboard.billList',
+          type,
+          size,
+          offset
+        },
+        success: ({ data }) => {
+          console.log('res:', data);
+          resolve(data);
+        },
+        fail(err) {
+          reject(err)
+        }
+      })
+    })
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: async function ({type}) {
+    console.log('type:',type,typeof type)
 
+    let data = await this.getData({type})
+    let datalist = [...this.data.datalist];
+    datalist.push(...data.song_list);
+
+    this.setData({
+      datalist,
+      type
+    })
+
+    // 设置页面标题
+    let current = app.globalData.types.filter(item=>item.type==type)[0]
+    wx.setNavigationBarTitle({
+      title: current.title
+    })
   },
 
   /**
@@ -53,8 +98,29 @@ Page({
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: async function () {
+    let {page,size,type,hasMore} = this.data;
+    if(!hasMore){
+      return;
+    }
+    page++;
 
+    // 计算offset
+    let offset = (page-1)*size
+
+    this.setData({
+      loading:true
+    })
+
+    let data = await this.getData({offset,type})
+    let datalist = [...this.data.datalist];
+    datalist.push(...data.song_list);
+    this.setData({
+      page,
+      datalist,
+      loading:false,
+      hasMore:data.song_list.length===size
+    })
   },
 
   /**
--- w8_Wechat/doc/readme.md
+++ w8_Wechat/doc/readme.md
@@ -31,5 +31,50 @@
     tabData = {1:[],2:[],11:[]}
     tabData['11']
     tabData['2'];
-
 ```
+
+## day8-5
+
+### 复习
+* 小程序开发使用的技术
+    * 组件
+        * swiper
+        * view
+        * text
+        * navigator
+        * 模板
+            * import/include
+            * template
+                * name
+                * is
+                * data
+    * API
+        * wx.request
+        * wx.navigateTo
+        * wx.getSystemInfo
+    * 往事件处理事件传参
+        * 利用自定义属性传参：data-xxx
+        * 获取：e.currentTarget.dataset.xxx
+    * 在小程序中使用async&await
+        * 引入 `const regeneratorRuntime = require("regenerator-runtime");`
+    * 在小程序中使用npm包
+        1. 勾选`使用npm模块`
+        2.  `工具` -> `构建npm`
+        3. `require('xxx')`
+* 扩展运算符使用场景
+    * `{...obj}`  把obj中所有属性扩展到对象中（等效于浅复制）
+    * `[...arr]`  把arr中所有数组元素扩展到数组中（等效于浅复制）
+    * `show(...params)`  函数调用是使用：params只能是数组，等效于`show(params[0],params[1],....)`
+    * `function show(a,...params){}`函数定义时使用：  params是一个数组，把除a以外的所有参数，写入params数组
+        * `show(10,20,30)`  结果：`a=10,params=[20,30]`
+        * `show(10)`  结果：`a=10,params=[]`
+
+* offset计算(size:5)
+[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+page    offset
+1       0
+2       5
+3       10
+4       15
+推导公式：offset = (page-1)*size
+    
